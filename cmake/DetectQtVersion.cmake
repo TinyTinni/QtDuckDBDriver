45d1697c76070f25412e35348305d3260324a31b
Matthias Möller <m_moeller@live.de>
2024-07-05 14:32:32 +0200

    trying to get more info about missing qt finders

--- cmake/DetectQtVersion.cmake
+++ cmake/DetectQtVersion.cmake
@@ -1,12 +1,15 @@
 # detect qt version
-find_package(Qt6 QUIET)
 set(_QT_VERSION 6)
+
+find_package(Qt6 QUIET COMPONENTS Core)
 if(NOT Qt6_FOUND)
-    message("Qt6 not found, trying Qt5")
-    find_package(Qt5 QUIET)
+    message(STATUS "Qt6 not found, trying Qt5")
+    find_package(Qt5 QUIET COMPONENTS Core)
     if (Qt5_FOUND)
         set(_QT_VERSION 5)
-        message("Qt5 found, using Qt5")
+        message(STATUS"Qt5 found, using Qt5")
+    else()
+        message(WARNING "Cannot find any Qt Version")
     endif()
 endif()
 

ce38fdcf10511aeef126650a9614c35b6ca1ec87
Matthias Möller <m_moeller@live.de>
2024-07-26 17:36:49 +0200

    add auto completion example

---
+++ examples/AutoCompletion/Readme.md
@@ -0,0 +1,7 @@
+# AutoCompletion
+
+![Preview](./preview.png)
+
+Creates a simple line edit widget with an attached [QCompleter](https://doc.qt.io/qt-6/qcompleter.html).
+Uses DuckDBs auto completion capabilities to complete the input.
+
---
+++ examples/AutoCompletion/main.cpp
@@ -0,0 +1,54 @@
+#include <QApplication>
+#include <QCompleter>
+#include <QLineEdit>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QStringListModel>
+
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
+
+	QCoreApplication::addLibraryPath(QCoreApplication::applicationDirPath() + "/plugins/");
+	QSqlDatabase db = QSqlDatabase::addDatabase("DUCKDB");
+
+	db.open();
+	db.exec("CREATE TABLE employee (Name VARCHAR, Salary INTEGER);");
+	db.exec("INSERT INTO employee VALUES ('Paul', 5000);");
+	db.exec("INSERT INTO employee VALUES ('Bert', 5500);");
+	db.exec("INSERT INTO employee VALUES ('Tina', 6500);");
+
+	QLineEdit *lineEdit = new QLineEdit();
+
+	auto *completer = new QCompleter(lineEdit);
+	lineEdit->setCompleter(completer);
+	completer->setCompletionMode(QCompleter::UnfilteredPopupCompletion);
+	completer->setCaseSensitivity(Qt::CaseSensitivity::CaseInsensitive);
+
+	auto model = new QStringListModel();
+	completer->setModel(model);
+
+	QObject::connect(lineEdit, &QLineEdit::textChanged, completer, [completer, db, model](QString text) {
+		QStringList prev = text.split(' ');
+		if (!prev.isEmpty())
+			prev.removeLast();
+		QString prevText = prev.join(' ');
+		if (!prevText.isEmpty())
+			prevText += ' ';
+
+		QSqlQuery query;
+		query.exec(QString("SELECT * FROM sql_auto_complete('%1');").arg(text));
+
+		QStringList list;
+		while (query.next()) {
+			QString result = query.value(0).toString();
+			QString completionText = prevText + result;
+			list.append(completionText);
+		}
+		model->setStringList(std::move(list));
+	});
+
+	lineEdit->show();
+	return app.exec();
+
+	return 0;
+}
